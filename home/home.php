ffef05ee6b3ad35f3a5063df60ec4a65168a6133
Thanh Quach <quachgioithanh@gmail.com>
2020-07-30 15:27:55 -0700

    Create home.php

---
+++ home/home.php
@@ -0,0 +1,10 @@
+<div class="bg">
+	<div class="cover"></div>
+	<div id="animated-intro">
+		<div class="d-flex flex-column justify-content-center h-100">
+		<img class="rounded mx-auto d-block" src="img/logo.png">
+		<div><p class="text-white f-size-2 text-center">Solution for Online learning</p></div>
+		</div>
+	</div>
+</div>
+<a href="#aboutus" id="scrollBtn" class="d-flex justify-content-center"><span class="fa fa-chevron-circle-down"></span></a>
