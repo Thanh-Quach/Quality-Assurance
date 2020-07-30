fdaa644b7ddd8e9e1bf30faf8a31c995f57a5880
Thanh Quach <quachgioithanh@gmail.com>
2020-07-30 15:31:18 -0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,16 @@
+<!-- Boostrap  Global Navigation Bar -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<title>Digi Class - Solution to online learning</title>
+
+	<link href="https://fonts.googleapis.com/css2?family=Paytone+One&family=Raleway&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<link rel="stylesheet" type="text/css" href="home/home.css">
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,8 @@
+<header><?php include('header.php')?></header>
+<div id="globalNav"><?php include('nav/globalNav.php'); ?></div>
+<div id="home"><?php include('home/home.php'); ?></div>
+<div id="aboutus"><?php include('about/aboutus.php'); ?></div>
+<div id="signup"><?php include('signUp/signup.php'); ?></div>
+<div id="footerNav"><?php include('nav/footerNav.php'); ?></div>
+<footer><?php include('footer.php')?></footer>
+	
