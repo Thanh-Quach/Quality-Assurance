8ce86f9a322c888196a1b2681a44001f1ca00863
Thanh Quach <quachgioithanh@gmail.com>
2020-07-30 15:30:45 -0700

    Update globalNav.php

--- nav/globalNav.php
+++ nav/globalNav.php
@@ -1,1 +1,24 @@
-
+<!-- Global Navigation -->
+<nav id="hideNav" class="navbar navbar-expand-lg custom-nav navbar-dark justify-content-between">
+  <a class="navbar-brand" href="#"><img id="logo" src="img/logo.png" width="120" alt=""></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div id="shadow"></div>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto justify-content-between" style="width: 480px;">
+      <li class="nav-item">
+        <a class="text-custom" href="#">What is Digi-Class?</a>
+      </li>
+      <li class="nav-item">
+        <a class="text-custom" href="#">Sign up</a>
+      </li>
+      <li class="nav-item">
+        <a class="text-custom" href="#">About Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="text-custom" href="#"></a>
+      </li>
+    </ul>
+  </div>
+</nav>
