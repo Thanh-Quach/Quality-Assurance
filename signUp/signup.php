d4cfc6ab34f0a8f4ac3dda73486412708d2ecced
Thanh Quach <quachgioithanh@gmail.com>
2020-08-01 01:33:35 -0700

    Add files via upload

--- signUp/signup.php
+++ signUp/signup.php
@@ -1,1 +1,34 @@
-
+<div id="signup" class="mx-auto d-flex flex-column justify-content-center">
+	<div class="text-center h-25 py-3 signup-title"><h1>Sign Up</h1></div>
+		<div class="row m-0">
+			<div id="signup-info" class="col-lg-6 col-sm-12 d-flex flex-column p-0">
+				<div id="content" class="w-75 mx-auto pb-3">
+					<p>Please let us know your information and we will contact you within 3 days. Details Information about tutoring group size and pricing can be found <a href="#signup" class="in-text">here</a>.</p>
+					<p>You can also <b>Contact Us</b> by</p>
+					<p>Phone: 0123-456-7890</p>
+					<p>Email: info@digiclass.com</p>
+					<p>Or let us <b>Contact You</b> by filling the form and Submit</p>
+				</div>
+			</div>
+			<div id="signup-form" class="col-lg-6 col-sm-12 d-flex flex-column justify-content-center p-0">
+				<form class="w-75 h-100 mx-auto d-flex flex-column justify-content-around">
+				  <div class="form-group">
+				    <input type="Name" class="form-control color-ctrl" id="name" placeholder="Name">
+				  </div>
+				  <div class="form-group">
+				    <input type="Grade" class="form-control color-ctrl" id="grade" placeholder="Student's grade">
+				  </div>
+				  <div class="form-group">
+				    <input type="Email" class="form-control color-ctrl" id="email" placeholder="Email">
+				  </div>
+				  <div class="form-group">
+				    <input type="phone" class="form-control color-ctrl" id="phone" placeholder="Phone">
+				  </div>
+				  <div class="form-group">
+				    <textarea class="form-control color-ctrl" id="Textarea" rows="3" placeholder="Your Request"></textarea>
+				  </div>
+				  <div class="d-flex justify-content-center"><button type="submit" class="btn-color-2 btn-lg w-50"><h1 class="m-0">Submit</h1></button></div>
+				</form>
+			</div>	
+	</div>
+</div>
