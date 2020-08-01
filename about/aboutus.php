95fc7add56f174fa162bf74ad7f3d999509b8ffa
Thanh Quach <quachgioithanh@gmail.com>
2020-08-01 01:30:56 -0700

    Add files via upload

--- about/aboutus.php
+++ about/aboutus.php
@@ -1,1 +1,60 @@
+<div id="aboutus" class="h-100">
+  <div id="aboutus-container" class="h-100">
+    <div id="aboutus-content" class="w-75 h-30 text-center mx-auto d-flex flex-column justify-content-center">
+      <h1>About Us</h1>
+      <p>Established in 2020, we are a start-up that will make tutoring service become simplier, more effective, and less expensive. Our core is being clear in our way of teaching, flexible in price with multiple group size and scheduling, and comfortable customer services.</p>
+    </div>
+    <div id="ourteam" class="text-center h-40">
+      <h3 class="pb-4">Meet our team</h3>
+      <div class="d-flex justify-content-around row m-0">
+        <div id="member" class="col-lg-4">
+        <div class="placeholder mx-auto"></div>
+        <h3>Name</h3><p>Title</p>
+        </div>
+        <div id="member" class="col-lg-4">
+        <div class="placeholder mx-auto"></div>
+        <h3>Name</h3><p>Title</p>
+        </div>
+        <div id="member" class="col-lg-4">
+        <div class="placeholder mx-auto"></div>
+        <h3>Name</h3><p>Title</p>
+        </div>
+      </div>
 
+    </div>
+    <div id="ourteam-responsive">
+      <h3 class="text-center pb-4">Meet our team</h3>
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div id="member" class="text-center">
+              <div class="placeholder mx-auto"></div>
+                <h3>Name</h3><p>Title</p>
+              </div>
+          </div>
+          <div class="carousel-item">
+             <div id="member" class="text-center">
+              <div class="placeholder mx-auto"></div>
+                <h3>Name</h3><p>Title</p>
+              </div>
+          </div>
+          <div class="carousel-item">
+            <div id="member" class="text-center">
+              <div class="placeholder mx-auto"></div>
+                <h3>Name</h3><p>Title</p>
+              </div>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
+      <a href="#signup" id="scrollBtn" class="w-100 hide-btn"><p class="text-center mb-0">Sign-up now</p><span class="fa fa-chevron-down d-flex justify-content-center"></span></a>
+  </div>
+</div>
