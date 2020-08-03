82b9b011330a221a5e6220dd8cc9a2cee571ba6e
Thanh Quach <quachgioithanh@gmail.com>
2020-08-03 14:31:10 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <header><?php include('header.php')?></header>
 <div id="globalNav"><?php include('nav/globalNav.php'); ?></div>
+<div id="socialMed"><?php include('nav/socialMed.php')?></div>
 <div id="home"><?php include('home/home.php'); ?></div>
 <div id="explain"><?php include('explain/explain.php'); ?></div>
 <div id="aboutus"><?php include('about/aboutus.php'); ?></div>
